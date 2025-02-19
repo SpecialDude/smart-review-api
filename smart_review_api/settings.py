db72be5e5423005684045198b40b2f05ab1a7788
Warith Adetayo <warith@encentralsolutions.com>
2025-02-19 17:03:52 +0100

    Smart notebook review

--- smart_review_api/settings.py
+++ smart_review_api/settings.py
@@ -146,5 +146,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost:5500",
     "http://localhost:5173",
-    "https://Lateefat319.pythonanywhere.com"
+    "https://Lateefat319.pythonanywhere.com",
+    'lateefat319.pythonanywhere.com'
 ]
